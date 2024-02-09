f1023a12a9cc2698da658ff35df9d14f8d3dd085
Wallace Kelly <wallace.kelly@digitalasset.com>
2024-02-09 11:34:25 -0500

    Remove commented code from setup.sql

--- setup.sql
+++ setup.sql
@@ -1,24 +1,9 @@
--- create or replace function pqs_contract_trigger_func() returns trigger as $$
--- declare
---   contract_row json;
--- begin
---   contract_row := json_build_object(
---     'table', TG_TABLE_NAME,
---     'action', TG_OP,
---     'pk', NEW.pk,
---     'contract_key', NEW.contract_key,
---     'payload', NEW.payload
---   );
---   perform pg_notify('pqs_contract_changes', contract_row::text);
---   return new;
--- end;
--- $$ language plpgsql;
-
 create or replace function pqs_watermark_trigger_func() returns trigger as $$
 declare
   watermark_row json;
 begin
   watermark_row := json_build_object(
+    -- these are the columns on the table
     'offset', NEW.offset,
     'ix', NEW.ix
   );
